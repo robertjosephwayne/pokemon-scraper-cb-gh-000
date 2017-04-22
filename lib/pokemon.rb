a3b1273e07330f5f69003bda3a980bc5c5cfba54
robertjosephwayne <robertjwayne@gmail.com>
2017-04-22 14:46:48 -0400

    Create find method to find a pokemon from the database using their id number

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,15 +1,21 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize(id=nil, name=nil, type=nil, db=nil)
-    @id = id
+  def initialize(name=nil, type=nil, db=nil)
     @name = name
     @type = type
     @db = db
   end
 
   def self.save(name, type, db)
-    ins = db.prepare('INSERT INTO pokemon (name, type) VALUES (?, ?)')
-    ins.execute([name, type])
+    db.execute('INSERT INTO pokemon (name, type) VALUES (?, ?)', name, type)
+  end
+
+  def self.find(id, db)
+    row = db.execute('SELECT id, name, type FROM pokemon WHERE id = (?)', id).first
+
+    pokemon = self.new(row[1], row[2], db)
+    pokemon.id = row[0]
+    pokemon
   end
 end
