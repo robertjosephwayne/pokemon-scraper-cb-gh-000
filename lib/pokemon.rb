1aeedb1e55f57b58675b398aa90a9ceac930cf1c
robertjosephwayne <robertjwayne@gmail.com>
2017-04-22 13:00:44 -0400

    Initialize Pokemon with id, name, type, and db

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,10 @@
 class Pokemon
+  attr_accessor :id, :name, :type, :db
+
+  def initialize(id=nil, name=nil, type=nil, db=nil)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+  end
 end
